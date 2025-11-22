dd6e894598fff2703d3a6200ae187e9651d5eb42
buiquocbinh811 <binhbinhbinh8112004@gmail.com>
2025-11-22 10:11:20 +0700

    cors permission

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -15,7 +15,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+import cors from 'cors';
+
+app.use(cors({
+  origin: [
+    'https://facebookkfc.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Serve static files (images)
